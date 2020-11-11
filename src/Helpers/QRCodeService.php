4bfa1312344073959a2f70874ca0e19bbd651bda
Chris West <me@christophermwest.co.uk>
2020-11-11 01:03:34 +0000

    Initial Commit

---
+++ src/Helpers/QRCodeService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SgPayNowQrCode\Helpers;
+
+class QRCodeService
+{
+    /**
+     * Generate a QR code
+     *
+     * @param string $string
+     * @return string
+     */
+    public static function generate(string $string, string $pathToImage): string
+    {
+        return base64_encode(\QrCode::format('png')
+                ->size(300)
+                ->style('square', 0.8)
+                ->margin(5)
+                ->errorCorrection('H')
+                ->mergeString($pathToImage, .2)
+                ->color(124, 26, 120)
+                ->generate($string));
+    }
+}
